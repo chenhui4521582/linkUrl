bb901e2da640d9c9af8a85b4deb6c80fbb2c5e62
yangwenlan <yangwenlan@patsnap.com>
2019-08-12 15:27:40 +0800

    add babel

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack')
+var path = require('path')
+const TerserPlugin = require('terser-webpack-plugin');
+module.exports={
+    entry:{
+        index:'./linkUrl.js',
+        server:'./server.js'
+    },
+    output:{
+        filename:'./[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    },
+    optimization: {
+        minimizer: [
+          //uglify js
+          new TerserPlugin({
+            test: /\.js(\?.*)?$/i,
+          }),
+        ],
+    }
+}
