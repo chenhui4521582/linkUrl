f88437ba1353c01bcad5e7842d5177fd4070e1cf
yangwenlan <yangwenlan@patsnap.com>
2019-08-12 17:34:24 +0800

    add linkurl

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports={
     mode: 'production',
     entry:{
-        index:'./linkUrl.js',
+      linkUrl:'./linkUrl.js',
         server:'./server.js'
     },
     output:{
